b9f95490787c10449eb6705df19d9fd0603b0995
Wanderson Braganca <wanderson.wbc@gmail.com>
2014-06-26 11:46:32 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,57 @@
+yii2-mmenu-widget
+=================
+
+The yii2-mmenu-widget widget is a Yii 2 wrapper for the [jQuery-mmenu](http://mmenu.frebsite.nl/):
+
+Installation
+------------
+
+The preferred way to install this extension is through [composer](http://getcomposer.org/download/).
+
+Either run
+
+```
+php composer.phar require --prefer-dist wbraganca/yii2-mmenu-widget "*"
+```
+
+or add
+
+```
+"wbraganca/yii2-mmenu-widget": "*"
+```
+
+
+How to use
+----------
+
+On your view file.
+
+```php
+<?php
+ echo \wbraganca\mmenu\Menu::widget([
+    'items' => [
+        [
+            'label' => 'Home',
+            'url' => '#',
+            'icon' => 'glyphicon glyphicon-home'
+        ],
+        [
+            'label' => 'Submenu',
+            'url' => '',
+            'icon' => 'glyphicon glyphicon-plus-sign',
+            'items' => [
+                [
+                    'label' => 'link 1',
+                    'url' => '#',
+                    'icon' => 'glyphicon glyphicon-file',
+                ],
+                 [
+                    'label' => 'link 2',
+                    'url' => '#',
+                    'icon' => 'glyphicon glyphicon-file',
+                ],
+            ]
+        ]
+    ]
+]); ?>
+```
---
+++ mmenu/Menu.php
@@ -0,0 +1,119 @@
+<?php
+/**
+ * @link      https://github.com/wbraganca/yii2-mmenu
+ * @copyright Copyright (c) 2014 Wanderson Bragança
+ * @license   https://github.com/wbraganca/yii2-mmenu/blob/master/LICENSE
+ */
+
+namespace wbraganca\mmenu;
+
+use Yii;
+use yii\base\InvalidConfigException;
+use yii\helpers\Html;
+use yii\helpers\Url;
+use yii\helpers\ArrayHelper;
+use yii\helpers\Json;
+
+/**
+ * The yii2-mmenu widget is a Yii 2 wrapper for the jQuery-mmenu.
+ * See more: http://mmenu.frebsite.nl/
+ *
+ * @author Wanderson Bragança <wanderson.wbc@gmail.com>
+ * @since 1.0.0
+ */
+class Menu extends \yii\widgets\Menu
+{
+    /**
+     * @var array
+     */
+    public $clientOptions = [];
+    /**
+     * @var string the template used to render the body of a menu which is NOT a link.
+     * In this template, the token `{label}` will be replaced with the label of the menu item.
+     * This property will be overridden by the `template` option set in individual menu items via [[items]].
+     */
+    public $labelTemplate = '{icon}{label}';
+    /**
+     * @var string the template used to render the body of a menu which is a link.
+     * In this template, the token `{url}` will be replaced with the corresponding link URL;
+     * while `{label}` will be replaced with the link text.
+     * This property will be overridden by the `template` option set in individual menu items via [[items]].
+     */
+    public $linkTemplate = '<a href="{url}">{icon}{label}</a>';
+    /**
+     * @var string
+     */
+    public $noLinkTemplate = '<span>{icon}{label}</span>';
+
+    public function init()
+    {
+        parent::init();
+        MenuAsset::register($this->getView());
+        $this->activateParents = true;
+    }
+
+    public function run()
+    {
+        echo Html::tag('nav', $this->renderMenu(), ['id' => $this->id]);
+        $this->registerScript();
+    }
+
+    /**
+     * Renders the main menu
+     */
+    protected function renderMenu()
+    {
+        if ($this->route === null && Yii::$app->controller !== null) {
+            $this->route = Yii::$app->controller->getRoute();
+        }
+        if ($this->params === null) {
+            $this->params = $_GET;
+        }
+        $items = $this->normalizeItems($this->items, $hasActiveChild);
+        $options = $this->options;
+        $tag = ArrayHelper::remove($options, 'tag', 'ul');
+        return Html::tag($tag, $this->renderItems($items), $options);
+    }
+
+    /**
+     * Renders the content of a side navigation menu item.
+     *
+     * @param array $item the menu item to be rendered. Please refer to [[items]] to see what data might be in the item.
+     * @return string the rendering result
+     * @throws InvalidConfigException
+     */
+    protected function renderItem($item)
+    {
+        $this->validateItems($item);
+        $url = ArrayHelper::getValue($item, 'url', '#');
+        $url = ($url === '') ? '' : Url::to($url);
+        $template = ($url === '') ? $this->noLinkTemplate : $this->linkTemplate;
+        $icon = empty($item['icon']) ? '' : '<i class="' . $item['icon'] . '"></i> &nbsp;';
+        return strtr($template, [
+            '{url}' => $url,
+            '{label}' => $item['label'],
+            '{icon}' => $icon
+        ]);
+    }
+
+    /**
+     * Validates each item for a valid label and url.
+     *
+     * @throws InvalidConfigException
+     */
+    protected function validateItems($item)
+    {
+        if (!isset($item['label'])) {
+            throw new InvalidConfigException("The 'label' option is required.");
+        }
+    }
+
+    protected function registerScript()
+    {
+        $id = $this->id;
+        $clientOptions = Json::encode($this->clientOptions);
+        $js = '$("nav#' . $id .'").mmenu(' . $clientOptions . ');';
+        $view = $this->getView();
+        $view->registerJs($js, \yii\web\view::POS_END);
+    }
+}
